852f78d944d71763550e6e0a5e692a81cf9722d3
Liza <170400eliza@gmail.com>
2021-12-04 21:36:39 +0300

    update

--- Simple-bot.py
+++ Simple-bot.py
@@ -6,10 +6,22 @@ from requests import get
 
 bot = telebot.TeleBot(token)
 
+cats= {'спит': 'Только не разбуди его!',
+       'вампир': "Прячься",
+       'летит':"Прикольно, правда?",
+       'ест':"Ты тоже иди покушай",
+       'наелся':"На самом деле он бы еще поел..."}
+
+photo= {'спит': "https://zagony.ru/uploads/posts/2014-12/1420023271_2.jpg" ,
+       'вампир': "https://medialeaks.ru/wp-content/uploads/2017/10/1-8-500x500.jpg",
+       'летит':"https://sun9-78.userapi.com/impg/88P8ehYFooObIqR48Ee5W_cJwIKiIMcDhhm4BA/6pIy90ODKz8.jpg?size=610x610&quality=96&sign=73065fd55961477677196d14cfe70349&type=album",
+       'ест':"https://pbs.twimg.com/media/Eqv_nUdXMAApd2i.jpg",
+       'наелся':"https://funart.pro/uploads/posts/2021-07/1625805370_8-funart-pro-p-samaya-tolstaya-koshka-zhivotnie-krasivo-f-8.jpg"}
+
+
 @bot.message_handler(commands=['start'])
 def start(message):
     keyboard = types.ReplyKeyboardMarkup()
-    keyboard.row("/help")
     keyboard.row("Привет")
     keyboard.row("Спит")
     keyboard.row("Вампир")
@@ -30,25 +42,19 @@ def start_message(message):
                                       '"Наелся" - появится котик, который уже покушал;\n\n'\
                                       'Удачи!')
 
+
+@bot.message_handler(commands=['day'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'О, ты хочешь узнать какой ты сегодня котик? Ну, лови :з')
+
+
 @bot.message_handler(content_types=['text'])
 def answer(message):
     if message.text.lower() == "привет":
         bot.send_message(message.chat.id, 'Привет! А я тут котиков показываю. Нажми на кнопочку "/help", если тебе нужна помощь, либо выбери, какого котика ты хочешь посмотреть.')
-    elif message.text.lower() == "спит":
-        bot.send_message(message.chat.id, "Только не разбуди его!")
-        bot.send_photo(message.chat.id, get("https://zagony.ru/uploads/posts/2014-12/1420023271_2.jpg").content)
-    elif message.text.lower() == "вампир":
-        bot.send_message(message.chat.id, "Прячься")
-        bot.send_photo(message.chat.id, get("https://medialeaks.ru/wp-content/uploads/2017/10/1-8-500x500.jpg").content)
-    elif message.text.lower() == "летит":
-        bot.send_message(message.chat.id, "Прикольно, правда?")
-        bot.send_photo(message.chat.id, get("https://sun9-78.userapi.com/impg/88P8ehYFooObIqR48Ee5W_cJwIKiIMcDhhm4BA/6pIy90ODKz8.jpg?size=610x610&quality=96&sign=73065fd55961477677196d14cfe70349&type=album").content)
-    elif message.text.lower() == "ест":
-        bot.send_message(message.chat.id, "Ты тоже иди покушай")
-        bot.send_photo(message.chat.id, get("https://pbs.twimg.com/media/Eqv_nUdXMAApd2i.jpg").content)
-    elif message.text.lower() == "наелся":
-        bot.send_message(message.chat.id, "На самом деле он бы еще поел...")
-        bot.send_photo(message.chat.id, get("https://funart.pro/uploads/posts/2021-07/1625805370_8-funart-pro-p-samaya-tolstaya-koshka-zhivotnie-krasivo-f-8.jpg").content)
+    elif message.text.lower() in cats:
+        bot.send_message(message.chat.id, cats.get( message.text.lower() ) )
+        bot.send_photo(message.chat.id, get( photo.get( message.text.lower()) ).content)
     else :
         bot.send_message(message.chat.id, "Если тебе требуется помощь, нто напиши /help.." )
 
